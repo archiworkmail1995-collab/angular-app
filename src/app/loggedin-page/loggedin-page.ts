e324599a32317c84a2c72bf19320f85fbca037ff
Ayush Bhatt <ayush.bhatt2@mail.dcu.ie>
2025-10-01 23:07:30 +0100

    initial commit

---
+++ src/app/loggedin-page/loggedin-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loggedin-page',
+  imports: [],
+  templateUrl: './loggedin-page.html',
+  styleUrl: './loggedin-page.css'
+})
+export class LoggedinPage {
+
+}
