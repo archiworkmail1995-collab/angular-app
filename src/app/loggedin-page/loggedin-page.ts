a29cb95879ff260813a907846ebc9b05442724d4
Archita Bahuguna <archi.workmail1995@gmail.com>
2025-10-01 23:52:01 +0100

    Adding angular material and Logged-in-page

--- src/app/loggedin-page/loggedin-page.ts
+++ src/app/loggedin-page/loggedin-page.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-loggedin-page',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule],
   templateUrl: './loggedin-page.html',
   styleUrl: './loggedin-page.css'
 })
